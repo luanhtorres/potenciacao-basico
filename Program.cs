8b383700963cee2e749d17322ab33266ede46f25
Luan Torres <100809026+luanhtorres@users.noreply.github.com>
2022-10-21 12:00:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace testeC_
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+             double numero_base;
+             double numero_potencia;
+             double resultado_potenciacao;
+        
+            Console.WriteLine("Insira o número base: ");
+            numero_base = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Insira o número da potencia: ");
+            numero_potencia = Convert.ToInt32(Console.ReadLine());
+            
+            resultado_potenciacao = Math.Pow(numero_base, numero_potencia);
+            
+            Console.WriteLine("O resultado de " + numero_base + " elevado a " + numero_potencia +  " é: " + resultado_potenciacao);
+            
+        }
+    }
+}
